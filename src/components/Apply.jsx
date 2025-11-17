72c2bbb5104c8f175d8b18d8f7d1c1c9cebf3c2c
Rishav <rishavkumar.nit@gmail.com>
2025-11-17 08:52:27 +0530

    customhook added

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -2,33 +2,72 @@ import React, { useContext, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import Footer from "./Footer";
 import { User } from "../App";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import Navbar from "./Navbar";
 import { jobs } from "./Jobs";
 import { addAppliedJob } from "./slices/JobSlice";
+import useForm from "./UseForm";
 
 export default function ApplyForm() {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const [step, setStep] = useState(1);
   const { id } = useParams();
-  const [submitted, setSubmitted] = useState(false);
-  const currentJob = jobs.find((item) => item.id.toString() === id.toString());
   const dispatch = useDispatch();
   const { profile } = useContext(User);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    experience: "",
-    skills: [],
-    skillInput: "",
-    coverLetter: "",
-    startDate: "",
-  });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const currentJob = jobs.find((item) => item.id.toString() === id.toString());
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const validations = {
+    1: (values) => {
+      const errors = {};
+      if (!values.name) errors.name = "Name is required";
+      if (!emailRegex.test(values.email)) errors.email = "Invalid email";
+      if (!values.phone || values.phone.length < 10)
+        errors.phone = "Invalid phone number";
+      return errors;
+    },
+
+    2: (values) => {
+      const errors = {};
+      if (!values.experience) errors.experience = "Experience required";
+      if (isNaN(values.experience))
+        errors.experience = "Experience must be a number";
+      return errors;
+    },
+
+    3: (values) => {
+      const errors = {};
+      if (!values.coverLetter) errors.coverLetter = "Enter a cover letter";
+      if (!values.startDate) errors.startDate = "Select a date";
+      if (new Date(values.startDate) < new Date())
+        errors.startDate = "Future date only";
+      return errors;
+    },
   };
+
+  const {
+    values: formData,
+    setValues: setFormData,
+    errors,
+    handleChange,
+    validateStep,
+    resetForm,
+  } = useForm(
+    {
+      name: "",
+      email: "",
+      phone: "",
+      experience: "",
+      skills: [],
+      skillInput: "",
+      coverLetter: "",
+      startDate: "",
+    },
+    validations
+  );
+
+  const [step, setStep] = useState(1);
+  const [submitted, setSubmitted] = useState(false);
 
   const addSkill = () => {
     if (formData.skillInput.trim() !== "") {
@@ -40,35 +79,19 @@ export default function ApplyForm() {
     }
   };
 
-  const handleStep2Button = () => {
-    if (formData.name === "") {
-      alert("Please complete the form");
-      return;
-    }
-    if (!emailRegex.test(formData.email)) {
-      alert("Enter a valid email");
-      return;
-    }
-    if (isNaN(Number(formData.phone)) || formData.phone.length < 10) {
-      alert("Enter a valid number");
-      return;
-    }
-    setStep(2);
+  const handleStep2 = () => {
+    if (validateStep(1)) setStep(2);
   };
 
-  const handleStep3Button = () => {
-    if (formData.experience === "") {
-      alert("Please add experience");
-      return;
-    }
-    if (isNaN(formData.experience)) {
-      alert("Pease add expeirence in numbers only");
-      return;
-    }
-    setStep(3);
+  const handleStep3 = () => {
+    if (validateStep(2)) setStep(3);
   };
 
-  const handleSaveApplication = () => {
+  const handleStep4 = () => {
+    if (validateStep(3)) setStep(4);
+  };
+
+  const handleSubmit = () => {
     dispatch(
       addAppliedJob({
         jobId: currentJob.id,
@@ -81,35 +104,9 @@ export default function ApplyForm() {
         brief: currentJob.brief,
       })
     );
-  };
 
-  const handleStep4Button = () => {
-    if (formData.coverLetter === "") {
-      alert("Cover letter cant be blank!!");
-      return;
-    }
-    const today = new Date();
-    const selected = new Date(formData.startDate);
-    if (selected < today) {
-      alert("Please enter future date only!!");
-      return;
-    }
-    setStep(4);
-  };
-
-  const handleSubmit = () => {
-    handleSaveApplication();
     setSubmitted(true);
-    setFormData({
-      name: "",
-      email: "",
-      phone: "",
-      experience: "",
-      skills: [],
-      skillInput: "",
-      coverLetter: "",
-      startDate: "",
-    });
+    resetForm();
   };
 
   return (
@@ -121,37 +118,38 @@ export default function ApplyForm() {
         {/* STEP 1 */}
         {step === 1 && (
           <div className="bg-white p-6 rounded-lg shadow w-full max-w-md">
-            <h2 className="text-xl font-semibold mb-4">
-              Step 1: Personal Information
-            </h2>
+            <h2 className="text-xl font-semibold mb-4">Step 1: Personal Info</h2>
 
             <input
               name="name"
               value={formData.name}
               onChange={handleChange}
               placeholder="Full Name"
-              className="w-full p-2 border rounded mb-3"
+              className="w-full p-2 border rounded mb-1"
             />
+            {errors.name && <p className="text-red-600 text-sm mb-2">{errors.name}</p>}
 
             <input
               name="email"
               value={formData.email}
               onChange={handleChange}
               placeholder="Email"
-              className="w-full p-2 border rounded mb-3"
+              className="w-full p-2 border rounded mb-1"
             />
+            {errors.email && <p className="text-red-600 text-sm mb-2">{errors.email}</p>}
 
             <input
               name="phone"
               value={formData.phone}
               onChange={handleChange}
               placeholder="Phone Number"
-              className="w-full p-2 border rounded mb-3"
+              className="w-full p-2 border rounded mb-1"
             />
+            {errors.phone && <p className="text-red-600 text-sm mb-2">{errors.phone}</p>}
 
             <button
-              className="bg-blue-600 text-white px-4 py-2 rounded mt-3 hover:cursor-pointer"
-              onClick={() => handleStep2Button()}
+              className="bg-blue-600 text-white px-4 py-2 rounded mt-3"
+              onClick={handleStep2}
             >
               Next
             </button>
@@ -168,10 +166,13 @@ export default function ApplyForm() {
               value={formData.experience}
               onChange={handleChange}
               placeholder="Years of Experience"
-              className="w-full p-2 border rounded mb-3"
+              className="w-full p-2 border rounded mb-1"
             />
+            {errors.experience && (
+              <p className="text-red-600 text-sm mb-2">{errors.experience}</p>
+            )}
 
-            {/* Add multiple skills */}
+            {/* Add skills */}
             <div className="flex gap-2 mb-3">
               <input
                 name="skillInput"
@@ -182,7 +183,7 @@ export default function ApplyForm() {
               />
               <button
                 onClick={addSkill}
-                className="bg-green-600 text-white px-3 py-2 rounded hover:cursor-pointer"
+                className="bg-green-600 text-white px-3 py-2 rounded"
               >
                 Add
               </button>
@@ -191,10 +192,7 @@ export default function ApplyForm() {
             {formData.skills.length > 0 && (
               <div className="mb-3 flex flex-wrap gap-2">
                 {formData.skills.map((s, i) => (
-                  <span
-                    key={i}
-                    className="bg-gray-300 px-2 py-1 rounded text-sm"
-                  >
+                  <span key={i} className="bg-gray-300 px-2 py-1 rounded text-sm">
                     {s}
                   </span>
                 ))}
@@ -203,14 +201,14 @@ export default function ApplyForm() {
 
             <div className="flex justify-between">
               <button
-                className="bg-gray-600 text-white px-4 py-2 rounded hover:cursor-pointer"
+                className="bg-gray-600 text-white px-4 py-2 rounded"
                 onClick={() => setStep(1)}
               >
                 Back
               </button>
               <button
-                className="bg-blue-600 text-white px-4 py-2 rounded hover:cursor-pointer"
-                onClick={() => handleStep3Button()}
+                className="bg-blue-600 text-white px-4 py-2 rounded"
+                onClick={handleStep3}
               >
                 Next
               </button>
@@ -221,97 +219,85 @@ export default function ApplyForm() {
         {/* STEP 3 */}
         {step === 3 && (
           <div className="bg-white p-6 rounded-lg shadow w-full max-w-md">
-            <h2 className="text-xl font-semibold mb-4">
-              Step 3: Additional Information
-            </h2>
+            <h2 className="text-xl font-semibold mb-4">Step 3: Additional Info</h2>
 
             <textarea
               name="coverLetter"
               value={formData.coverLetter}
               onChange={handleChange}
               placeholder="Cover Letter"
-              className="w-full p-2 border rounded mb-3"
+              className="w-full p-2 border rounded mb-1"
               rows={4}
             />
+            {errors.coverLetter && (
+              <p className="text-red-600 text-sm mb-2">{errors.coverLetter}</p>
+            )}
 
             <input
               type="date"
               name="startDate"
               value={formData.startDate}
               onChange={handleChange}
-              className="w-full p-2 border rounded mb-3"
+              className="w-full p-2 border rounded mb-1"
             />
+            {errors.startDate && (
+              <p className="text-red-600 text-sm mb-2">{errors.startDate}</p>
+            )}
 
             <div className="flex justify-between">
               <button
-                className="bg-gray-600 text-white px-4 py-2 rounded hover:cursor-pointer"
+                className="bg-gray-600 text-white px-4 py-2 rounded"
                 onClick={() => setStep(2)}
               >
                 Back
               </button>
               <button
-                className="bg-green-600 text-white px-4 py-2 rounded hover:cursor-pointer"
-                onClick={() => handleStep4Button()}
+                className="bg-green-600 text-white px-4 py-2 rounded"
+                onClick={handleStep4}
               >
                 Proceed
               </button>
             </div>
           </div>
         )}
+
+        {/* STEP 4 SUMMARY */}
         {step === 4 && (
           <div className="bg-white p-6 rounded-lg shadow w-full max-w-md">
-            <h2 className="text-xl font-semibold m-2">Application summary</h2>
-            <p>
-              <span className="font-semibold m-2">Name: </span>
-              {formData.name}
-            </p>
-            <p>
-              <span className="font-semibold m-2">Email: </span>
-              {formData.email}
-            </p>
-            <p>
-              <span className="font-semibold m-2">Phone: </span>
-              {formData.phone}
-            </p>
-            <p>
-              <span className="font-semibold m-2">Experience: </span>
-              {formData.experience}
-            </p>
-            <p>
-              <span className="font-semibold m-2">Skills: </span>
-              {formData.skills}
-            </p>
-            <p>
-              <span className="font-semibold m-2">Cover letter: </span>
-              {formData.coverLetter}
-            </p>
-            <p>
-              <span className="font-semibold m-2">Start date: </span>
-              {formData.startDate}
-            </p>
+            <h2 className="text-xl font-semibold m-2">Application Summary</h2>
 
-            <div className="flex justify-between mt-2">
+            {Object.entries(formData).map(([key, value]) => (
+              key !== "skillInput" && (
+                <p key={key}>
+                  <span className="font-semibold capitalize">{key}:</span>{" "}
+                  {Array.isArray(value) ? value.join(", ") : value}
+                </p>
+              )
+            ))}
+
+            <div className="flex justify-between mt-3">
               <button
-                className="bg-gray-600 text-white px-4 py-2 rounded hover:cursor-pointer"
+                className="bg-gray-600 text-white px-4 py-2 rounded"
                 onClick={() => setStep(3)}
               >
                 Edit Application
               </button>
               <button
-                className="bg-green-600 text-white px-4 py-2 rounded hover:cursor-pointer"
-                onClick={() => handleSubmit()}
+                className="bg-green-600 text-white px-4 py-2 rounded"
+                onClick={handleSubmit}
               >
                 Submit
               </button>
             </div>
           </div>
         )}
+
+        {/* success toast */}
         <Link to={"/applications"}>
           <div
-            className={`fixed bottom-12 right-2 p-2 bg-green-500
-             rounded text-white shadow-lg transition-opacity duration-700
-            ${submitted ? "opacity-100" : "opacity-0"}
-          `}
+            className={`fixed bottom-12 right-2 p-2 bg-green-500 rounded text-white shadow-lg transition-opacity duration-700 ${
+              submitted ? "opacity-100" : "opacity-0"
+            }`}
           >
             Application submitted
           </div>
